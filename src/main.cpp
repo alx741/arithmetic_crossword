bc55abd2c9d47cd7fd52a40a4409e1f7559f4754
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-05-12 01:52:12 -0500

    Add complete `game.hpp` interface example

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,41 @@
 
 using namespace std;
 
+/* TODO: Debug only function */
+void debug(const vector< vector<int> >& matrix)
+{
+    for(int i=0; i<matrix.size(); i++)
+    {
+        for(int j=0; j<matrix[0].size(); j++)
+        {
+            if( matrix[i][j] == -1)
+            {
+                cout << "." << " | ";
+            }
+            else
+            {
+                cout << matrix[i][j] << " | ";
+            }
+        }
+
+        cout << endl;
+    }
+    cout << endl << endl;
+}
+void debug(const vector<query_t>& queries)
+{
+    cout << endl << endl;
+    for(int i=0; i<queries.size(); i++)
+    {
+        cout << "Query at  " << queries[i].x << "," << queries[i].y << " / direction: ";
+        cout << queries[i].dir << " / Operation: " << queries[i].operation << " = ";
+        cout << queries[i].result;
+        cout << endl;
+    }
+    cout << endl << endl;
+}
+
+
 
 int main()
 {
@@ -40,12 +75,51 @@ int main()
     }
     */
 
-    if( create_game(10, 10, 50, DIV | ADD | MUL | SUB) )
+
+    /*
+     * Due to entropy some random generated parameters can cause a fail when
+     * allocation is possible, so call this funcion 5 or more times just to be
+     * sure it is impossible to allocate
+     */
+    if( create_game(10, 10, 40, DIV | ADD | MUL | SUB) )
     {
-        cout << "[ ! ] Creado con exito" << endl << endl;
-    }else{ cout << "FALLO la creacion" << endl << endl; }
+        cout << "Creado con exito" << endl << endl;
+    }
+    else
+    {
+        cout << "[ ! ]FALLO la creacion" << endl << endl;
+    }
 
 
 
+    /*
+     * Retrieve a list of available gamesets to play and their descriptions
+     */
+    vector<gameset_desc_t> gameset_desc_vect;
+    if( get_gamesets(gameset_desc_vect) )
+    {
+        cout << "Si existen gamesets" << endl;
+        cout << "El ID del primer game set es: " << gameset_desc_vect[0].gameset_id << endl;
+        cout << "Y su descripcion: \n" << gameset_desc_vect[0].game_description << endl;
+    }
+    else
+    {
+        cout << "[ ! ]FALLO, no hay gamesets disponibles" << endl << endl;
+    }
 
+
+    /*
+     * Leats load the first game of GAMESET_DESC_VECT obtained before
+     * This will fill up GAME_MATRIX  and GAME_QUERIES so we are ready to play!
+     *
+     * Using the very first gameset (index 0)
+     */
+    load_game(gameset_desc_vect[0].gameset_id);
+
+
+    /*
+     * Printing GAME_MATRIX and GAME_QUERIES so we can see the result
+     */
+    debug(GAME_MATRIX);
+    debug(GAME_QUERIES);
 }
